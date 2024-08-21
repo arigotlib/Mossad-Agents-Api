1dc487fdc80fa2bf8879617c5d6b70abf51ed8b9
Arie Gotlib <ag0548472624@gmail.com>
2024-08-21 16:16:41 +0300

    Add project files.

---
+++ MossadAgentsApi/Models/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace MossadAgentsApi.Models
+{
+    internal class Agent
+    {
+        [Key]
+        public int Id { get; set; }
+        public string photo_url { get; set; }
+        public string nickname { get; set; }
+        public int? x { get; set; }
+        public int? y { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ MossadAgentsApi/Models/Target.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MossadAgentsApi.Models
+{
+    internal class Target
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string position { get; set; }
+        public string? photo_url { get; set; }// שמתי סימן שאלה כדי לאפשר לזה ליהיות ריק כיוון שבאפיון יש סתירה בין הישות לבין מה שנשלח מהסמולציה
+        public int? x {  get; set; }
+        public int? y { get; set; }
+    }
+}
