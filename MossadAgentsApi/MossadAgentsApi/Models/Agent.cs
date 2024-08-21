355309ab5bcf063a5f9765d7e2188b8834554707
arigotlib <ag0548472624@gmail.com>
2024-08-21 14:33:58 +0300

    Add files via upload

---
+++ MossadAgentsApi/MossadAgentsApi/Models/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace MossadAgentsApi.Models
+{
+    internal class Agent
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Nickname { get; set; }
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ MossadAgentsApi/MossadAgentsApi/Models/Target.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MossadAgentsApi.Models
+{
+    internal class Target
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+    }
+}
